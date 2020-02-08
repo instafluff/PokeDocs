f7cbfbfca3b63146d83dd8fbdd9bac2f2d289e80
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-02-07 21:50:04 -0800

    PokeVision Recognition

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require( "dotenv" ).config();
+
 const fs = require( "fs" );
 const fetch = require( "node-fetch" );
 
@@ -85,7 +87,148 @@ function allPokemonKnowledgeBasesAreBelongToUs() {
 	// generateKnowledgeBase( "pokedata_G8", 810, 890 );
 }
 
-allPokemonKnowledgeBasesAreBelongToUs();
+// allPokemonKnowledgeBasesAreBelongToUs();
+
+
+
+// Code to download the pokemon images
+const generationNames = [ "red-blue", "silver", "ruby-sapphire", "diamond-pearl", "black-white", "x-y" ];
+const pokemonImageUrlFormat = `https://img.pokemondb.net/sprites/GENERATION/normal/POKEMON.png`;
+const imagePath = "pokevision";
+const download = require('image-downloader');
+
+async function downloadPokemonImages( numPokemon, startIndex = 0 ) {
+	let data = await retrievePokeData( numPokemon, startIndex );
+	// console.log( data );
+	data.forEach( p => {
+		generationNames.forEach( (g, i) => {
+			let url = pokemonImageUrlFormat.replace( "GENERATION", g ).replace( "POKEMON", p.info.name );
+			download.image({
+			  url: url,
+			  dest: `${imagePath}/${("00" + p.info.id).slice(-3)}_${g}.png`
+		  	});
+		});
+	} )
+	// console.log( data );
+}
+// downloadPokemonImages( 151, 0 );
+
+
+
+// Code to upload & tag images
+const util = require('util');
+const TrainingApiClient = require("@azure/cognitiveservices-customvision-training");
+const PredictionApiClient = require("@azure/cognitiveservices-customvision-prediction");
+
+const setTimeoutPromise = util.promisify(setTimeout);
+
+const trainingKey = process.env.CUSTOM_VISION_KEY;
+const predictionKey = process.env.CUSTOM_VISION_KEY;
+const predictionResourceId = process.env.CUSTOM_VISION_RESOURCE_ID;
+const endPoint = process.env.CUSTOM_VISION_ENDPOINT;
+const projectId = process.env.CUSTOM_VISION_PROJECT_ID;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function createTagsForPokemon( numPokemon, startIndex = 0 ) {
+	let data = await retrievePokeData( numPokemon, startIndex );
+	let tagData = await fetch( `${endPoint}/customvision/v3.0/training/projects/${projectId}/tags`, {
+						headers: {
+							"Training-Key": trainingKey,
+						}
+					} )
+					.then( r => r.json() );
+	let tags = {};
+	tagData.forEach( t => tags[ t.name ] = t );
+	let timeIndex = 0;
+	let promises = data.map( async p => {
+		if( !tags[ p.info.name ] ) {
+			await sleep( 1000 * timeIndex++ ); // Prevent Rate Limit
+			// Create a new tag
+			let tagData = await fetch( `${endPoint}/customvision/v3.0/training/projects/${projectId}/tags?name=${p.info.name}`, {
+								method: "POST",
+								headers: {
+									"Training-Key": trainingKey,
+								}
+							} )
+							.then( r => r.json() );
+			// console.log( tagData );
+			tags[ p.info.name ] = tagData;
+		}
+	} );
+	await Promise.all( promises );
+	console.log( tags );
+}
+
+// NOTE: Free Tier only supports max of 50 tags! S0 Tier supports up to 500.
+// createTagsForPokemon( 50, 0 );
+
+async function deleteTagsForPokemon( numPokemon, startIndex = 0 ) {
+	let tagData = await fetch( `${endPoint}/customvision/v3.0/training/projects/${projectId}/tags`, {
+						headers: {
+							"Training-Key": trainingKey,
+						}
+					} )
+					.then( r => r.json() );
+	console.log( tagData );
+	let promises = tagData.map( async t => {
+		let tagData = await fetch( `${endPoint}/customvision/v3.0/training/projects/${projectId}/tags/${t.id}`, {
+							method: "DELETE",
+							headers: {
+								"Training-Key": trainingKey,
+							}
+						} )
+						.then( r => r.json() );
+	} );
+	await Promise.all( promises );
+}
+// deleteTagsForPokemon();
+
+async function uploadPokemonImages( tags, numPokemon, startIndex = 0 ) {
+	let data = await retrievePokeData( numPokemon, startIndex );
+	// console.log( data );
+	for( var d = 0; d < data.length; d++ ) {
+		let p = data[ d ];
+		for( var i = 0; i < generationNames.length; i++ ) {
+			let g = generationNames[ i ];
+			let filename = `${imagePath}/${("00" + p.info.id).slice(-3)}_${g}.png`;
+			let tag = tags[ p.info.name ];
+			// console.log( tags, p.info.name );
+			await sleep( 1000 ); // Prevent hitting 2 transactions per 1 sec rate limit
+			let result = await fetch( `${endPoint}/customvision/v3.0/training/projects/${projectId}/images?tagIds[]=${tag.id}`, {
+								method: "POST",
+								body: fs.readFileSync(filename),
+								headers: {
+									"Training-Key": trainingKey,
+									'Content-Type': 'application/octet-stream',
+								}
+							} )
+							.then( r => r.json() );
+							// console.log( result.images[ 0 ].image );
+		};
+	};
+}
+
+async function uploadPokemon() {
+	let tagData = await fetch( `${endPoint}/customvision/v3.0/training/projects/${projectId}/tags`, {
+						headers: {
+							"Training-Key": trainingKey,
+						}
+					} )
+					.then( r => r.json() );
+	let tags = {};
+	tagData.forEach( t => tags[ t.name ] = t );
+
+	for( var i = 0; i < 50; i++ ) {
+		// console.log( "Uploading: " + i );
+		await uploadPokemonImages( tags, 1, i );
+	}
+}
+
+uploadPokemon();
+
 
 // const Web = require( "webwebweb" );
 // Web.Run( 12920 );
